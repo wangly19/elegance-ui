cf172cd1d2f5562e440c3a9a5bccf4d230ec8ecf
wangly19 <18607948581@163.com>
2020-04-10 02:43:16 +0800

    Doc: 文档打包，国内源

--- docs/docs/.vuepress/config.js
+++ docs/docs/.vuepress/config.js
@@ -2,7 +2,7 @@ module.exports = {
   theme:"antdocs",
   title: "Elegance-UI Color UI",
   description: "A lightweight creator for VuePress project.",
-  base: "/docs/docs/.vuepress/dist",
+  base: "/elegance-ui/docs",
   head: [
     ["link",{ rel: "icon",href: "/assets/logo.png" }]
   ],
